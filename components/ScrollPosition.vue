05b332a836b5866af2a95c10c98f0dee58e66f90
Elian Remaggi <elianremaggi@gmail.com>
2025-05-14 16:59:48 -0300

    FEAT ( Scroll Position )
    Add new component to track all the web de scroll position used only in development

---
+++ components/ScrollPosition.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useLenis } from 'lenis/vue'
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const scrollPosition = ref(0)
+const lenis = useLenis()
+
+// Función handler para el scroll
+const scrollHandler = ({ scroll }) => {
+    scrollPosition.value = Math.round(scroll)
+}
+
+onMounted(() => {
+    // Usamos el método .on de la instancia lenis
+    if (lenis.value) {
+        lenis.value.on('scroll', scrollHandler)
+    }
+})
+
+onUnmounted(() => {
+    // Usamos el método .destroy para limpiar
+    if (lenis.value) {
+        lenis.value.destroy()
+    }
+})
+</script>
+
+<template>
+    <div class="fixed bottom-4 right-4 z-50 p-3 bg-black bg-opacity-70 text-white rounded-lg">
+        Scroll position: {{ scrollPosition }}px
+    </div>
+</template>
