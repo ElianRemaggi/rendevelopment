78d4b5dd099fbd33e22174a054461cf3adce42b5
Elian Remaggi <elianremaggi@gmail.com>
2025-05-15 19:18:33 -0300

    REFACTOR  ( tittle )
    deleted props, now de scrollValue is injected

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@ onUnmounted(() => {
 
     <div class="wrapper w-full pb-16 ">
         <div class="md:py-12 lg:py-16 xl:py-20">
-            <Tittle :scrollValue="scrollPosition" />
+            <Tittle/>
             <services :scrollValue="scrollPosition" />
             <!--
 
